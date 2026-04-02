d66495c1e4e565a26bdfd912ac8e68d9afd6b551
konocompany <konocompanymultiservicse@gmail.com>
2026-04-02 00:50:54 -0400

    Update server configuration and hero section

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const http = require('http'); 
+const { Server } = require('socket.io');
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
 
 // MIDDLEWARE
 app.use(cors({
@@ -21,6 +24,33 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      'https://radiotelemegastar.netlify.app',
+      'https://radio-tele-megastar.pages.dev',
+      'http://localhost:3000',
+      'http://127.0.0.1:5500'
+    ],
+    methods: ["GET", "POST"]
+  }
+});
+let onlineUsers = 0;
+
+io.on('connection', (socket) => {
+  console.log('🟢 User connecté');
+
+  onlineUsers++;
+  io.emit('onlineCount', onlineUsers);
+
+  socket.on('disconnect', () => {
+    console.log('🔴 User déconnecté');
+
+    onlineUsers--;
+    io.emit('onlineCount', onlineUsers);
+  });
+});
 
 // KONEKSYON MONGODB
 mongoose.connect(process.env.MONGODB_URI)
@@ -48,7 +78,7 @@ app.get('/', (req, res) => {
 
 // DÉMÀRAJ SÈVÈ
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🚀 Sèvè ap kouri sou pò ${PORT}`);
   console.log(`📻 Radio Télé Mega Star Backend prè!`);
 });
